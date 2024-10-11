bc7bdc7418977cc90b20a7867385338e47357650
Jona Schwarz <jonaschwarz1@gmail.com>
2024-10-11 11:32:14 +0200

    require flake8 and eslint_d from none-ls

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -3,6 +3,9 @@ return {
 	-- still need to install linters and formatters with mason
 	-- :Mason   =>    /linter_name <CR>     =>      i (to install linter/fromatter)
 	"nvimtools/none-ls.nvim",
+      dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+    },
 	config = function()
 		local null_ls = require("null-ls")
 
@@ -11,9 +14,11 @@ return {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.isort,
 				null_ls.builtins.formatting.black.with({ extra_args = { "--line-length", "180" } }),
-				null_ls.builtins.diagnostics.flake8.with({ extra_args = { "--max-line-length", "180" } }),
+				-- null_ls.builtins.diagnostics.flake8.with({ extra_args = { "--max-line-length", "180" } }),
+                require("none-ls.diagnostics.flake8"),
+                require("none-ls.diagnostics.eslint_d"),
 				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.diagnostics.eslint_d,
+				-- null_ls.builtins.diagnostics.eslint_d,
 				null_ls.builtins.formatting.rust_analyzer,
 			},
 		})
